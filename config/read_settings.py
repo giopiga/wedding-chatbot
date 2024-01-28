e5ff0c8728421276d0bb7091de8a6cf3744c7713
Giovanni Pigani <giovanni.pigani@moxoff.com>
2024-01-28 22:35:32 +0100

    feat: add new functionalities, manage password and change chatbot

---
+++ config/read_settings.py
@@ -0,0 +1,21 @@
+
+def read_key():
+    try:
+        with open('config/key.txt') as f:
+            lines = f.readlines()
+            key = lines[0]
+            return key
+    except Exception as e:
+        raise Exception(f'No key found because of {e}')
+    
+
+def read_pwd():
+    try:
+        with open('config/pwd.txt') as f:
+            lines = f.readlines()
+            pwd = lines[0]
+            return pwd
+    except Exception as e:
+        raise Exception(f'No pwd found because of {e}')
+
+
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import os
 import random
 import streamlit as st
+from config.read_settings import read_key, read_pwd
 
+content_firts_message = "Benvenuto! Sono il wedding chatbot. Come posso aiutarti?"
 #decorator
 def enable_chat_history(func):
     if os.environ.get("OPENAI_API_KEY"):
@@ -20,7 +22,7 @@ def enable_chat_history(func):
 
         # to show chat history on ui
         if "messages" not in st.session_state:
-            st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+            st.session_state["messages"] = [{"role": "assistant", "content": content_firts_message}]
         for msg in st.session_state["messages"]:
             st.chat_message(msg["role"]).write(msg["content"])
 
@@ -38,13 +40,13 @@ def display_msg(msg, author):
     st.session_state.messages.append({"role": author, "content": msg})
     st.chat_message(author).write(msg)
 
+    
 def configure_openai_api_key():
-    openai_api_key = st.sidebar.text_input(
-        label="OpenAI API Key",
-        type="password",
-        value=st.session_state['OPENAI_API_KEY'] if 'OPENAI_API_KEY' in st.session_state else '',
-        placeholder="sk-..."
-        )
+    # in prod and local:
+    if 'openai_api_key' in st.secrets:
+        # read from secrets toml or from streamlit secrets
+        openai_api_key = st.secrets['openai_api_key']
+
     if openai_api_key:
         st.session_state['OPENAI_API_KEY'] = openai_api_key
         os.environ['OPENAI_API_KEY'] = openai_api_key
@@ -52,4 +54,42 @@ def configure_openai_api_key():
         st.error("Please add your OpenAI API key to continue.")
         st.info("Obtain your key from this link: https://platform.openai.com/account/api-keys")
         st.stop()
-    return openai_api_key
+    return
+
+question_main = "Per poter accedere, ti chiediamo di rispondere alla domanda!"
+question = "Come mi chiamo?"
+def insert_password():
+
+    # in prod and local
+    if 'pwd' in st.secrets:
+        pwd = st.secrets['pwd']
+
+    if 'ASK_PWD' not in st.session_state or st.session_state['ASK_PWD']==True:
+        password_inserted = st.sidebar.text_input(
+            label=question,
+            type="password",
+            value=st.session_state['PWD'] if 'PWD' in st.session_state else '',
+            placeholder="nome"
+            )
+        
+        if password_inserted.lower() == pwd.lower():
+            st.session_state['PWD'] = password_inserted
+            st.sidebar.success('Risposta corretta! Puoi iniziare a chattare!')
+            # when the correct pwd is inserted, we remove the question
+            st.session_state['ASK_PWD'] = False
+            return True
+        
+        elif password_inserted.lower() is None or password_inserted.lower()=='':
+            # to handle the case that the password is not been written yet
+            st.error(question_main)
+            st.stop()
+        
+        else:
+            st.error(question_main)
+            st.sidebar.error("Risposta errata")
+            st.stop()
+ 
+    else:
+        pass
+
+
