e4126a48a2f183b0f40d2106e80c9f888e7e516a
Giovanni Pigani <giovanni.pigani@moxoff.com>
2024-02-21 22:51:10 +0100

    manage password, unique page, theme settings

--- Home.py
+++ Home.py
@@ -1,9 +1,98 @@
+import os
+import utils
 import streamlit as st
+from streaming import StreamHandler
 
-st.set_page_config(
-    page_title="Langchain Chatbot",
-    page_icon='💬',
-    layout='wide'
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.memory import ConversationBufferMemory
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+st.header("Chatta con il Wedding Chatbot!")
+st.write("""Il chatbot risponderà alle tue domande riguardo il matrimonio!""")
+st.write(
+    """Ti chiediamo di non fare troppe domande, in quanto il numero di interazioni è limitato! 
+         Grazie!"""
 )
 
-st.header("VAI ALLA PAGINA WEDDING CHAT SUL MENU. QUESTA PAGINA SARA' RIMOSSA")
+
+class CustomDataChatbot:
+
+    def __init__(self):
+        utils.configure_openai_api_key()
+        self.openai_model = "gpt-3.5-turbo"
+        utils.insert_password()
+
+    def save_file(self, file):
+        folder = "tmp"
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        file_path = f"./{folder}/{file.name}"
+        with open(file_path, "wb") as f:
+            f.write(file.getvalue())
+        return file_path
+
+    @st.spinner(
+        "Un giorno, il marinaio spiegò le vele al vento, ma il vento non capì..."
+    )
+    def setup_qa_chain(self):
+        # Load documents
+        docs = []
+
+        file_path = "Wedding Chatbot Info.pdf"
+        loader = PyPDFLoader(file_path)
+        docs.extend(loader.load())
+
+        # Split documents
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1500, chunk_overlap=200
+        )
+        splits = text_splitter.split_documents(docs)
+
+        # Create embeddings and store in vectordb
+        embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+        vectordb = DocArrayInMemorySearch.from_documents(splits, embeddings)
+
+        # Define retriever
+        retriever = vectordb.as_retriever(
+            search_type="mmr", search_kwargs={"k": 2, "fetch_k": 4}
+        )
+
+        # Setup memory for contextual conversation
+        memory = ConversationBufferMemory(
+            memory_key="chat_history", return_messages=True
+        )
+
+        # Setup LLM and QA chain
+        llm = ChatOpenAI(model_name=self.openai_model, temperature=0, streaming=True)
+        qa_chain = ConversationalRetrievalChain.from_llm(
+            llm, retriever=retriever, memory=memory, verbose=True
+        )
+        return qa_chain
+
+    @utils.enable_chat_history
+    def main(self):
+
+        user_query = st.chat_input(placeholder="Chiedimi qualcosa!")
+
+        if user_query:
+            qa_chain = self.setup_qa_chain()
+
+            utils.display_msg(user_query, "user")
+
+            with st.chat_message("assistant"):
+                st_cb = StreamHandler(st.empty())
+                response = qa_chain.run(user_query, callbacks=[st_cb])
+                st.session_state.messages.append(
+                    {"role": "assistant", "content": response}
+                )
+
+
+if __name__ == "__main__":
+    obj = CustomDataChatbot()
+    obj.main()
--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import os
-import random
 import streamlit as st
 from config.read_settings import read_key, read_pwd
 
 content_firts_message = "Benvenuto! Sono il wedding chatbot. Come posso aiutarti?"
-#decorator
+
+
 def enable_chat_history(func):
     if os.environ.get("OPENAI_API_KEY"):
 
@@ -22,13 +22,17 @@ def enable_chat_history(func):
 
         # to show chat history on ui
         if "messages" not in st.session_state:
-            st.session_state["messages"] = [{"role": "assistant", "content": content_firts_message}]
+            st.session_state["messages"] = [
+                {"role": "assistant", "content": content_firts_message}
+            ]
         for msg in st.session_state["messages"]:
             st.chat_message(msg["role"]).write(msg["content"])
 
     def execute(*args, **kwargs):
         func(*args, **kwargs)
+
     return execute
+
 
 def display_msg(msg, author):
     """Method to display message on the UI
@@ -40,56 +44,57 @@ def display_msg(msg, author):
     st.session_state.messages.append({"role": author, "content": msg})
     st.chat_message(author).write(msg)
 
-    
+
 def configure_openai_api_key():
     # in prod and local:
-    if 'openai_api_key' in st.secrets:
+    if "openai_api_key" in st.secrets:
         # read from secrets toml or from streamlit secrets
-        openai_api_key = st.secrets['openai_api_key']
+        openai_api_key = st.secrets["openai_api_key"]
 
     if openai_api_key:
-        st.session_state['OPENAI_API_KEY'] = openai_api_key
-        os.environ['OPENAI_API_KEY'] = openai_api_key
+        st.session_state["OPENAI_API_KEY"] = openai_api_key
+        os.environ["OPENAI_API_KEY"] = openai_api_key
     else:
         st.error("Please add your OpenAI API key to continue.")
-        st.info("Obtain your key from this link: https://platform.openai.com/account/api-keys")
+        st.info(
+            "Obtain your key from this link: https://platform.openai.com/account/api-keys"
+        )
         st.stop()
     return
 
+
 question_main = "Per poter accedere, ti chiediamo di rispondere alla domanda!"
 question = "Come mi chiamo?"
-def insert_password():
+question_asked = "Per poter accedere, scrivi come mi chiamo!"
 
-    # in prod and local
-    if 'pwd' in st.secrets:
-        pwd = st.secrets['pwd']
 
-    if 'ASK_PWD' not in st.session_state or st.session_state['ASK_PWD']==True:
-        password_inserted = st.sidebar.text_input(
-            label=question,
-            type="password",
-            value=st.session_state['PWD'] if 'PWD' in st.session_state else '',
-            placeholder="nome"
-            )
-        
-        if password_inserted.lower() == pwd.lower():
-            st.session_state['PWD'] = password_inserted
-            st.sidebar.success('Risposta corretta! Puoi iniziare a chattare!')
-            # when the correct pwd is inserted, we remove the question
-            st.session_state['ASK_PWD'] = False
-            return True
-        
-        elif password_inserted.lower() is None or password_inserted.lower()=='':
-            # to handle the case that the password is not been written yet
-            st.error(question_main)
-            st.stop()
-        
+def check_password():
+    """Returns `True` if the user had the correct password."""
+    if "pwd" in st.secrets:
+        pwd = st.secrets["pwd"]
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        # if hmac.compare_digest(st.session_state["password"], pwd):
+        if st.session_state["password"].lower() == pwd.lower():
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
         else:
-            st.error(question_main)
-            st.sidebar.error("Risposta errata")
-            st.stop()
- 
-    else:
-        pass
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        question_asked, type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password errata! Riprova!")
+    return False
 
 
+def insert_password():
+    if not check_password():
+        st.stop()
